9bdb7eaffd4c3ba0d788627b753491d28a79494b
KavitaSharma <sharma97kavita@gmail.com>
2024-11-07 00:40:48 +0000

    refactor

--- src/pages/Contact/Form/ContactForm.jsx
+++ src/pages/Contact/Form/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useState } from 'react'
 import emailjs from '@emailjs/browser'
 import './contact-form.scss'
 import Loader from 'react-loaders'
